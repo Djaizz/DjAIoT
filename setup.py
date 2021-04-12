ed64a5f918124c3ed0b8f8de262598084f81b1cf
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-12 15:26:14 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import json
+from setuptools import find_namespace_packages, setup
+from types import SimpleNamespace
+
+
+metadata = SimpleNamespace(**json.load(open('metadata.json')))
+
+
+setup(
+    name=metadata.PACKAGE,
+    version=metadata.VERSION,
+    description=metadata.DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author=metadata.AUTHOR,
+    author_email=metadata.AUTHOR_EMAIL,
+    maintainer=metadata.AUTHOR,
+    maintainer_email=metadata.AUTHOR_EMAIL,
+    url=metadata.URL,
+    download_url=metadata.DOWNLOAD_URL,
+    packages=find_namespace_packages(include=['djai.*']),
+    py_modules=[],
+    scripts=[],
+    classifiers=metadata.CLASSIFIERS,
+    license='MIT',
+    keywords=metadata.KEYWORDS,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=open('requirements.txt').readlines(),
+    entry_points={},
+    extras_require={},
+    python_requires='>= 3.7',
+    setup_requires=[],
+    namespace_packages=['djai']
+)
