e1cd4f67b2ba4ca6852328d7044ad517e0f8bb8d
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-14 01:12:15 -0700

    add djai.data.models.pandas.PandasDataFrame

--- djai/data/models/__init__.py
+++ djai/data/models/__init__.py
@@ -1,3 +1,4 @@
 from .base import DataSchema, DataSet
 from .json import JSONDataSet, NamedJSONDataSet
 from .numpy import NumPyArray, NamedNumPyArray
+from .pandas import PandasDataFrame, NamedPandasDataFrame
