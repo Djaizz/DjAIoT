3179caac0399aa3f61756d034f1feeffa4d00c8f
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-14 17:03:33 -0700

    fix metadata path in djai

--- src/djai/__init__.py
+++ src/djai/__init__.py
@@ -1,8 +1,10 @@
 import json
+from pathlib import Path
 from types import SimpleNamespace
 
 
-metadata = SimpleNamespace(**json.load(open('metadata.json')))
+metadata = SimpleNamespace(**json.load(open(Path(__file__).parent /
+                                            'metadata.json')))
 
 
 __version__ = metadata.VERSION
