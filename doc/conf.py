669be9438fd98cf5625c2faaa9247e95dfb259ff
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-20 13:17:07 -0700

    add Markdown-related deps for doc

--- doc/conf.py
+++ doc/conf.py
@@ -78,3 +78,10 @@ html_theme = 'alabaster'
 # They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+source_suffix = {
+    '.md': 'markdown',
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown'
+}
