a6620a4988ed32656c496301673409a6914fec69
Vinh Luong <MBALearnsToCode@UChicago.edu>
2021-04-20 12:49:35 -0700

    update doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -8,8 +8,9 @@
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+# add these directories to sys.path here.
+# If the directory is relative to the documentation root,
+# use os.path.abspath to make it absolute, like shown here.
 
 # import os
 # import sys
@@ -28,9 +29,9 @@ release = '0.0.0.dev3'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings.
+# They can be extensions coming with Sphinx (named 'sphinx.ext.*')
+# or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',   # Include documentation from docstrings
     'sphinx.ext.autosectionlabel',   # Allow reference sections using its title
@@ -60,20 +61,20 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
+# List of patterns, relative to source directory,
+# that match files and directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# The theme to use for HTML and HTML Help pages.
+# See the documentation for a list of builtin themes.
 html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
+# relative to this directory.
+# They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
