02115ab83a7ef18f38c49ca609b5a7f6a386b640
STEAM for Vietnam Foundation: AI & Robotics Educational Initiative <Vinh@STEAMforVietNam.org>
2021-11-22 22:36:32 -0800

    add basic packaging files

---
+++ src/djaiot/__init__.py
@@ -0,0 +1,16 @@
+"""DjAIoT package."""
+
+
+from importlib.metadata import version
+import sys
+
+if sys.version_info >= (3, 9):
+    from collections.abc import Sequence
+else:
+    from typing import Sequence
+
+
+__all__: Sequence[str] = ('__version__',)
+
+
+__version__: str = version(distribution_name='DjAIoT')
